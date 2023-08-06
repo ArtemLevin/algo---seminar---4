099b060fed163c36695c577e0f732c2390c80a82
ArtemLevin <python_study_2022@mail.ru>
2023-08-06 21:07:00 +0300

    RBT almost done

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,5 @@
 import java.awt.*;
 
-class HashMap{
-
-    class Entity{
-        int key;
-        int value;
-    }
-    class Basket{
-        Node head;
-        class Node{
-            Entity entity;
-            Node next;
-        }
-
-        int find(int key){
-            Node  node = head;
-            while(node != null){
-                if(node.entity.key == key){
-                    return  node.entity.value;// use comparator if compare objects
-                }
-                node = node.next;
-            }
-
-            return -1;
-        }
-
-        boolean push(Entity entity){
-            Node node = new Node();
-            node.entity = entity;
-
-            if(head == null){
-                head = node;
-            }
-            else{
-                Node cur = head;
-                while(cur.next != null){
-                    if(cur.entity.key == entity.key){
-                        return false;
-                    }
-                    cur = cur.next;
-                }
-                cur.next = node;
-            }
-            return true;
-        }
-
-    }
-    static final int INIT_SIZE = 16;
-    Basket[] baskets;
-
-    public HashMap(){
-        this(INIT_SIZE);
-    }
-    public HashMap(int size){
-        baskets = new Basket[size];
-    }
-
-    int getIndex(int key){
-        // key.hashCode();
-        return key % baskets.length;
-    }
-
-    int find(int key){
-        int index = getIndex(key);
-        Basket basket = baskets[index];
-        if(basket != null) {
-            return basket.find(key);
-        }
-        return -1;
-    }
-
-    boolean push(int key, int value){
-        Entity entity = new Entity();
-        entity.key = key;
-        entity.value = value;
-        int index = getIndex(key);
-        Basket basket = baskets[index];
-        if(basket == null){
-            basket = new Basket();
-            baskets[index] = basket;
-        }
-        return basket.push(entity);
-
-    }
-
-}
-
-
 class BinaryTree{
     Node root;
     class Node {
@@ -95,79 +8,24 @@ class BinaryTree{
         Node right;
         private Color color;
     }
-    boolean push(int value){
-        if(root == null){
-            root = new Node();
-            root.value = value;
-//            reb
-            return true;
-        }
-        Node node = root;
-        while(node != null){
-            if(node.value == value){
-                return false;
-            }
-            if(node.value < value) {
-                if (node.right == null) {
-                    node.right = new Node();
-                    node.right.value = value;
-//reb
-                    return true;
-                } else {
-                    node = node.right;
-                }
-            }else{
-                if(node.left ==null){
-                    node.left = new Node();
-                    node.left.value = value;
-//                    reb
-                    return true;
-                }
-                else{
-                    node = node.left;
-                }
-            }
-        }
-        return false;
-
-    }
-
-    boolean find(int value){
-        Node node = root;
-        while(node != null){
-            if(node.value == value){
-                return true;
-            }
-            if(node.value < value) {
-                node = node.right;
-            }
-            else {
-                node = node.right;
-                }
-        }
-        return false;
-
-    }
-
-}
 
     private boolean addNode(Node node, int value){
         if(node.value == value){
             return false;
         }
         else{
-            if(node.value > value){
-                if(node.left != null){
+            if(node.value > value) {
+                if (node.left != null) {
                     boolean result = addNode(node.left, value);
                     node.left = rebalance(node.left);
                     return result;
-                    }
-                else{
-                    node.left = new Node;
+                } else {
+                    node.left = new Node();
                     node.left.color = Color.RED;
                     node.left.value = value;
                     return true;
                 }
+            }
             else{
                 if(node.right != null){
                     boolean result = addNode(node.right, value);
@@ -180,7 +38,7 @@ class BinaryTree{
                     node.right.value = value;
                     return true;
                 }
-                }
+
             }
         }
     }
@@ -194,7 +52,8 @@ class BinaryTree{
     private Node leftSwap(Node node){
         Node left = node.left;
         Node between = left.right;
-        left.right = between;
+        left.right = node;
+        node.left = between;
         left.color = node.color;
         node.color = Color.RED;
         return left;
@@ -206,7 +65,7 @@ class BinaryTree{
         right.left = node;
         node.right = between;
         right.color = node.color;
-        node.color = color.RED;
+        node.color = Color.RED;
         return right;
     }
 
@@ -215,7 +74,7 @@ class BinaryTree{
         boolean needRebalance;
         do{
             needRebalance = false;
-            if(result.right != null && result.right.color == color.RED &&
+            if(result.right != null && result.right.color == Color.RED &&
                     (result.left == null || result.left.color == Color.BLACK)){
                 needRebalance = true;
                 result = rightSwap(result);
